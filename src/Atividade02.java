484c01e4d3040d411d237f7a8047d205c591c14d
JonathanBrunetto <168039735+JonathanBrunetto@users.noreply.github.com>
2024-09-04 19:58:20 -0300

    Aprendendo Vetores

---
+++ src/Atividade02.java
@@ -0,0 +1,27 @@
+
+public class Atividade02 {
+    public static void main(String[] args) {
+
+        int votoPessoa1 = 10;
+        int votoPessoa2 = 23;
+        int votoPessoa3 = 10;
+        int votoPessoa4 = 10;
+        int votoPessoa5 = 99;
+        int votoPessoa6 = 10;
+        int votoPessoa7 = 99;
+        int votoPessoa8 = 10;
+        int votoPessoa9 = 23;
+        int votoPessoa10 = 99;
+
+        //primeira forma de criar e inicializar vetor
+        //criamos o vetor vazio
+        int[] votos = new int[10];
+
+        //nomeVetor[posiçao] = valor
+        //Vetor vai da posiçao 0 até 9
+        votos[0] = 90;
+        votos[1] = 10;
+        votos[9] = 90;
+
+    }
+}
